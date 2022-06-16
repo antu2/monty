768cf577edd5b2f4c0243470a51ac989b32efad3
antu2 <antenehmulugetaboss12@gmail.com>
2022-06-17 01:11:38 +0300

    all in one

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-ALX Monty
+# 0x18. C - Stacks, Queues - LIFO, FIFO
+
+## Description
+In this project we are tasked to create an interpreter for Monty ByteCode files. These files will have commands per line to manipulate the data structure given.
+
+What you should learn from this project:
+
+* What do LIFO and FIFO mean
+* What is a stack, and when to use it
+* What is a queue, and when to use it
+* What are the common implementations of stacks and queues
+* What are the most common use cases of stacks and queues
+* What is the proper way to use global variables
+* How to work with git submodules
+
+---
+
+## Instructions
+
+Compile with:
+```
+gcc -Wall -Werror -Wextra -pedantic *.c -o monty
+```
+---
+
+## Example
+
+![demo](https://puu.sh/CksXw/b90dd61cb5.png)
+
+---
+
+## Files
+
+### [monty.c](./monty.c)
+* Contains the main function that takes in the file and runs the parser.
+
+### [monty.h](./monty.h)
+* Header file.
+---
+
+## Requirements
+- All your files will be compiled on Ubuntu 14.04 LTS
+- Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
+- You allowed to use a maximum of one global variable
+- No more than 5 functions per file
+- You are allowed to use the C standard library
+- The repository monty should be added as a submodule to your holbertonschool-low_level_programming repository, under the name 0x18-stacks_queues_lifo_fifo
+
+---
+
+## Tasks
+
+### 0. push, pall
+* Implement the push and pall opcodes.
+* The opcode push pushes an element to the stack.
+* The opcode pall prints all the values on the stack, starting from the top of the stack.
+
+### 1. pint
+* Implement the pint opcode.
+* The opcode pint prints the value at the top of the stack, followed by a new line.
+
+### 2. pop
+* Implement the pop opcode.
+* The opcode pop removes the top element of the stack.
+
+### 3. swap
+* Implement the swap opcode
+* The opcode swap swaps the top two elements of the stack.
+
+### 4. add
+* Implement the add opcode.
+* The opcode add adds the top two elements of the stack.
+
+### 5. nop
+* Implement the nop opcode.
+* The opcode nop doesn’t do anything.
+
+### Advanced Tasks
+* opcodes: sub, div, mul, mod, comments, pchar, pstr, rotl, rotr, stack, queue, Holberton, Add two digits, Multiplication, Multiplication level up
--- monty.h
+++ monty.h
@@ -1,19 +1,39 @@
-#ifndef MONTY
-#define MONTY
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
+ *  * struct stack_s - doubly linked list representation of a stack (or queue)
+ *   * @n: integer
+ *    * @prev: points to the previous element of the stack (or queue)
+ *     * @next: points to the next element of the stack (or queue)
+ *      *
+ *       * Description: doubly linked list node structure
+ *        * for stack, queues, LIFO, FIFO Holberton project
+ *         */
+typedef struct arg_s
+{
+	int arg;
+	int flag;
+} arg_t;
+
+extern arg_t arg;
+
+/**
+ *  * struct stack_s - doubly linked list representation of a stack (or queue)
+ *   * @n: integer
+ *    * @prev: points to the previous element of the stack (or queue)
+ *     * @next: points to the next element of the stack (or queue)
+ *      *
+ *       * Description: doubly linked list node structure
+ *        * for stack, queues, LIFO, FIFO Holberton project
+ *         */
 typedef struct stack_s
 {
 	int n;
@@ -22,13 +42,13 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
- */
+ *  * struct instruction_s - opcode and its function
+ *   * @opcode: the opcode
+ *    * @f: function to handle the opcode
+ *     *
+ *      * Description: opcode and its function
+ *       * for stack, queues, LIFO, FIFO Holberton project
+ *        */
 typedef struct instruction_s
 {
 	char *opcode;
@@ -36,42 +56,69 @@ typedef struct instruction_s
 } instruction_t;
 
 /**
- * struct var_global - structure with the first addres of elements
- * @first: Pointer to stack
- * @buffer: The buffer
- * @file: The file descriptor of fopen.
- */
-typedef struct var_global
+ *  * struct line - contents of line and corresponding number
+ *   * @contents: array of tokens read from the line
+ *    * @number: the line number
+ *     *
+ *      * Description: contents of a line and corresponding number
+ *       */
+typedef struct line
 {
-	stack_t **first;
-	char *buffer;
+	unsigned int number;
+	char **content;
+} line_t;
+
+/**
+ *  * struct stack_s - doubly linked list representation of a stack (or queue)
+ *   * @n: integer
+ *    * @prev: points to the previous element of the stack (or queue)
+ *     * @next: points to the next element of the stack (or queue)
+ *      *
+ *       * Description: doubly linked list node structure
+ *        * for stack, queues, LIFO, FIFO Holberton project
+ *         */
+typedef struct meta_s
+{
+	char *buf;
+	stack_t *stack;
 	FILE *file;
-} global_t;
-extern global_t header;
+} meta_t;
 
-/* Main functions */
-void start_stack(stack_t **stack);
-void function_select(stack_t **stack, unsigned int line_number, char *command);
-void find_file(char *path, stack_t **stack);
-int manage_error(char *value, unsigned int line_number);
+/* Important functions */
+void (*get_op_func(line_t line, meta_t *meta))(stack_t **, unsigned int);
+int _isalpha(int c);
 
-/* Command functions */
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
-void _div(stack_t **stack, unsigned int line_number);
-void _mul(stack_t **stack, unsigned int line_number);
-void _mod(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void _rotl(stack_t **stack, unsigned int line_number);
-void _rotr(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
+/* Parse functions */
+void parsefile(FILE *file);
+void parseline(line_t *line, char *buffer);
 
-void free_all(void);
+/* Verification functions */
+bool comment_check(line_t line);
+void push_check(line_t line, meta_t *meta, char *opcode);
 
-#endif
+/* Stack manipulation functions */
+void push(stack_t **stack, unsigned int nline);
+void pall(stack_t **stack, unsigned int nline);
+void pint(stack_t **stack, unsigned int nline);
+void pop(stack_t **stack, unsigned int nline);
+void swap(stack_t **stack, unsigned int nline);
+void nop(stack_t **stack, unsigned int nline);
+void rotl(stack_t **stack, unsigned int nline);
+void rotlop(stack_t **stack, unsigned int nline);
+void rotrop(stack_t **stack, unsigned int nline);
+void pchar(stack_t **stack, unsigned int nline);
+void pstr(stack_t **stack, unsigned int nline);
+void free_stack(stack_t **stack);
+void nop(stack_t **stack, unsigned int nline);
+void qpush(stack_t **stack, unsigned int nline);
+void addqu(stack_t **stack, unsigned int nline);
+void addst(stack_t **stack, unsigned int nline);
+
+/* Math functions */
+void subop(stack_t **stack, unsigned int nline);
+void addop(stack_t **stack, unsigned int nline);
+void divop(stack_t **stack, unsigned int nline);
+void mulop(stack_t **stack, unsigned int nline);
+void modop(stack_t **stack, unsigned int nline);
+
+#endif /* MONTY_H */
